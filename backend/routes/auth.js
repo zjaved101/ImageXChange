4f2a86c521c51acb7d5daaeda10f713fafa9b9e8
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-07-01 13:38:30 -0700

    add error handling for duplicate entry in DB

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,18 +3,14 @@ const router = express.Router();
 const db = require('../config/db');
 
 /* GET users listing. */
-router.post('/signup', function(req, res, next) {
-  // db.query("SELECT * FROM User", function(err, rows, fields) {
-  //   console.log(rows);
-  // });
-  // console.log('request: ', req.body);
-  // console.log(`request: ${req.body.username}`);
-  // res.send('This is the signup post route');
-  
-  console.log('request: ', req.body);
+router.post('/signup', function(req, res, next) {  
   db.query("INSERT INTO User(username,password,email) VALUES(?,?,?)", [req.body.username, req.body.password, req.body.email], function (err, results, fields) {
-    console.log(err);
-    res.send(results);
+    if(err && err['code'] === 'ER_DUP_ENTRY') {
+      // Set http status code and return message
+      res.status(400);
+      res.send('duplicate entry detected');
+    }
+    res.send(200);
   });
 });
 
