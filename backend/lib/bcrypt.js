0de10520f4c4f3c88794cdf0bfc5331cff637b3b
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-07-07 15:54:02 -0700

    added compare function and renamed functions

--- backend/lib/bcrypt.js
+++ backend/lib/bcrypt.js
@@ -1,23 +1,47 @@
 const bcrypt = require('bcrypt');
 
-async function hashPassword(password) {
+async function hash(string) {
   try {
-    const hashPassword = await new Promise(function(resolve, reject) {
-      bcrypt.hash(password, 10, function(err, hash) {
+    const hashString = await new Promise(function(resolve, reject) {
+      bcrypt.hash(string, 10, function(err, hash) {
         if(err)
           reject(err);
         
           resolve(hash);
       });
     });
-    
-    return hashPassword;
+
+    return hashString;
   } catch(err) {
-    console.log(err);
+    console.log("hashString -> ", err);
     return '';
   }
 }
 
+async function compare(string, hash) {
+  try {
+    const compareString = await new Promise(function(resolve, reject) {
+      bcrypt.compare(string, hash, function(err, result) {
+        if(err) {
+          console.log("bcrypt.compare -> ", err);
+          reject(false);
+        }
+
+        if(result)
+          resolve(true);
+        
+        reject(false);
+      });
+    });
+
+    return compareString;
+  } catch(err) {
+    console.log("compareString -> ", err);
+    return false;
+  }
+}
+
 module.exports = {
-  hashPassword: hashPassword
+  hash: hash,
+  compare: compare
 }
