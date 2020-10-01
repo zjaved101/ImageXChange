cab48df79023f906dade0fa3ca4c9f75450af779
Zan Javed <zjaved101@gmail.com>
2020-10-01 13:23:13 -0700

    route to purchase image (#6)
    Co-authored-by: Zan Javed <{ID}+{username}@users.noreply.github.com>

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -17,4 +17,17 @@ router.post('/upload', function(req, res, next) {
     // return res.send('error with upload');
 });
 
+router.post('/buy', function(req, res, next) {
+  db.query("INSERT INTO Transaction(user_id,img_id) VALUES(?,?)", [req.body.user_id, req.body.img_id], function (err, results, fields) {
+    console.log(results);
+    if(err && err['code'] === 'ER_DUP_ENTRY') {
+      // Set http status code and return message for duplicate entry in DB
+      res.status(400);
+      return res.send('duplicate entry detected');
+    }
+
+    return res.send('Successfully bought image');
+  });
+});
+
 module.exports = router;
